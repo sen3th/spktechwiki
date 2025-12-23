64ee772b91eb0fa1ed7dc6f9c88863ca05280e2f
Seneth <hi@seneth.me>
2025-12-22 20:32:42 -0500

    theme customization

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {
             href: 'https://github.com/sen3th/spktechwiki',
