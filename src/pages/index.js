54c24b1c823b38c71cd254024f32b3fffbf05b71
Seneth <hi@seneth.me>
2025-12-22 21:15:50 -0500

    added button fluidity, image resize

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,21 @@
 // File: src/pages/my-page.js
 import React from 'react';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import styles from './index.module.css';
 
 export default function MyPage() {
   return (
     <Layout title="SPK Tech Team Wiki" description="Documentation and tutorials for equipment and events">
-      <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <main className={styles.main}>
         <h1>SPK Tech Team Wiki</h1>
         <p>Documentation and tutorials for equipment and events</p>
-        <button
-          style={{
-            padding: '0.5rem 1rem',
-            fontSize: '1rem',
-            borderRadius: '4px',
-            border: 'none',
-            backgroundColor: '#4CAF50',
-            color: 'white',
-            cursor: 'pointer',
-          }}
-          onClick={() => {window.location.href = '/docs/intro';}}
-        >
+        <Link className={styles.getStartedButton} to="/docs/intro">
           Get Started
-        </button>
+        </Link>
         <br/>
         <br/>
-        <img src = "/img/homepage-gif.gif" alt="homepage gif" style={{ marginTop: '2rem', width: '200px' }}/>
+        <img src = "/img/homepage-gif.gif" alt="homepage gif" className={styles.heroImage}/>
       </main>
     </Layout>
   );
