ba684413b08a667a3f5052b5cb5d64a3f0b8a215
Seneth <hi@seneth.me>
2026-01-02 12:01:31 -0500

    algolia fix #1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,7 +145,7 @@ const config = {
       indexName: 'spk-tech-wiki',
 
       // Optional: see doc section below
-      contextualSearch: true,
+      contextualSearch: false,
     },
     }),
 };
